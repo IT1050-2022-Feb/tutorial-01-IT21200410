1b967377a1600d481e777fa29cdfd3445696d4ff
IT21200410 <89298903+IT21200410@users.noreply.github.com>
2022-02-09 06:10:13 +0000

    completed ex1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,25 @@
 #include <stdio.h>
 
 int main() {
+
+
+   // Variables declarations 
+  int mark1 = 0, mark2 = 0;
+  float avg = 0;
+
+  // Asking the two marks from the user
+  printf("Enter mark 1: ");
+  scanf("%d", &mark1);
+
+  printf("Enter mark 2: ");
+  scanf("%d", &mark2);
+
+  // Calculating average of the two marks
+  avg  = (mark1 + mark2)/ 2.0;
   
+  // Printing average to the user
+  printf("\n\nThe average of the two marks is %.2f\n", avg);
+
   return 0;
 }
 
