f0c3276f8ff0c627ca8830b2166ccbd924a6e11f
IT21200410 <89298903+IT21200410@users.noreply.github.com>
2022-02-09 12:03:20 +0000

    Completed ex3

--- Tute03.c
+++ Tute03.c
@@ -12,7 +12,24 @@ n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
+
 int main() {
+  
+  // Variable declarations
+  int n = 0, i = 0, sum = 0;
+
+  // Asking input n
+  printf("Enter number to calculate sum: ");
+  scanf("%d", &n);
+
+  // Using for loop to calculate sum from 1 to n
+  for (i = 1; i <=n ; i++)
+  {
+      sum += i;
+  }
+
+  // Displaying the sum from 1 to n to the user
+  printf("The sum from 1 to %d is %d\n", n, sum );
   
   return 0;
 }
